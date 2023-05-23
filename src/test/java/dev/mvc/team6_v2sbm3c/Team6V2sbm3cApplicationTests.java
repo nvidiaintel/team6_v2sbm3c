cd8621e52e26391fe95c621aad3c15af6da6d6ef
nvidiaintel <downloadchoice@gmail.com>
2023-05-23 15:03:41 +0900

    첫번째 커밋

---
+++ src/test/java/dev/mvc/team6_v2sbm3c/Team6V2sbm3cApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mvc.team6_v2sbm3c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team6V2sbm3cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
